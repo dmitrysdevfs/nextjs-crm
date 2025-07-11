3c4edade4ca09e77ed16c55f2ff351aec6184069
Dmytro <d.salyakin@gmail.com>
2025-07-11 23:57:57 +0300

    Add status labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import ActiveLabel from "./components/active-label";
+import NotActiveLabel from "./components/not-active-label";
 
 
 export default function Home() {
   return (
     <main >
       <h1>Home page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
 
   );
