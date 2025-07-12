ac5bf9b141e04435bd0d5f04d11efa14cc91d45c
Dmytro <d.salyakin@gmail.com>
2025-07-12 23:29:05 +0300

    Add company form

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React, { useState } from 'react';
+import Button from '@/app/components/button';
+import dynamic from 'next/dynamic';
+// import CompanyFormModal from '@/app/components/company-form-modal';
+
+const CompanyFormModal = dynamic(
+  () => import('@/app/components/company-form-modal'),
+  { ssr: false },
+);
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
