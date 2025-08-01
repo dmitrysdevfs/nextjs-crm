4d4b07fc2afe68f889aa2e5742600ef001e49d07
Dmytro <d.salyakin@gmail.com>
2025-08-02 01:12:36 +0300

    Add companies page

---
+++ src/app/companies/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '@/app/components/header';
+
+export interface PageProps {
+  params: { id: string };
+}
+
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  return (
+    <>
+      <Header>Companies ({id})</Header>
+      <p>{new Date().toTimeString()}</p>
+    </>
+  );
+}
