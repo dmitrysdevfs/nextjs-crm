995dbbcdaa2c51b326992c46c566471cbc08d1a6
Dmytro <d.salyakin@gmail.com>
2025-08-02 03:45:23 +0300

    Add error handling components

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
 import Header from '@/app/components/header';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
-
 
 export default async function Page({ params }: PageProps) {
   const { id } = await params;
+
+  const idNum = Number.parseInt(id);
+  if (Number.isNaN(idNum)) {
+    notFound();
+  }
+
   return (
     <>
       <Header>Companies ({id})</Header>
-      <p>{new Date().toTimeString()}</p>
     </>
   );
 }
